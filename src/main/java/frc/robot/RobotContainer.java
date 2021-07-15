64ca36cdca2dd82fec76be7202fb75f13c11bd05
HaleyPimley <joe.derrick@interlakes.org>
2021-07-15 10:44:09 -0400

    First commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.commands.*;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import frc.robot.subsystems.SwerveDrivetrain;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  private final XboxController controller = new XboxController(0);
+
+  private final SwerveDrivetrain drivetrain = new SwerveDrivetrain();
+
+  private final CommandBase SwerveTest = new AutoSwerveDriveCommand(drivetrain, controller);
+
+  SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    drivetrain.setDefaultCommand(new SwerveDriveCommand(drivetrain, controller));
+    m_chooser.setDefaultOption("Swerve Test", SwerveTest);
+  }
+
+  public Command getAutonomousCommand(){
+    return m_chooser.getSelected();
+  }
+}
